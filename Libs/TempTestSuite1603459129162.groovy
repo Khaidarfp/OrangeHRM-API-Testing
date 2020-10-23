33ee1c6c24b1bcc3e32edd3694891fb4eeff0ac4
khaidarfp <khaidar.febrianto@gmail.com>
2020-10-23 20:43:17 +0700

    perubahan gitignore

---
+++ Libs/TempTestSuite1603459129162.groovy
@@ -0,0 +1,39 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/OrangeHRM_API_Testing')
+
+suiteProperties.put('name', 'OrangeHRM_API_Testing')
+
+suiteProperties.put('description', '')
+
+suiteProperties.put('stopImmediately', 'false')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("D:\\Dokumen\\Katalon Studio Project\\SP3 OrangeHRM API Testing\\Reports\\20201023_201849\\OrangeHRM_API_Testing\\20201023_201849\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/OrangeHRM_API_Testing', suiteProperties, new File("D:\\Dokumen\\Katalon Studio Project\\SP3 OrangeHRM API Testing\\Reports\\20201023_201849\\OrangeHRM_API_Testing\\20201023_201849\\testCaseBinding"))
